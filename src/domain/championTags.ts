707da735f07f34a37f493e0de7894408130591db
Rain <heyu.520@qq.com>
2026-04-13 19:56:33 +0800

    feat:扩展英雄属性筛选

--- src/domain/championTags.ts
+++ src/domain/championTags.ts
@@ -64,6 +64,10 @@ const PROFESSION_TAGS = new Set([
 const ACQUISITION_TAGS = new Set(['cneoriginal', 'core', 'event', 'evergreen', 'starter', 'tales'])
 
 const TAG_LABELS: Record<string, { 'zh-CN': string; 'en-US': string }> = {
+  aarakocra: {
+    'zh-CN': '阿拉寇克拉',
+    'en-US': 'Aarakocra',
+  },
   aasimar: {
     'zh-CN': '亚斯玛',
     'en-US': 'Aasimar',
@@ -80,6 +84,14 @@ const TAG_LABELS: Record<string, { 'zh-CN': string; 'en-US': string }> = {
     'zh-CN': '吟游诗人',
     'en-US': 'Bard',
   },
+  bullywug: {
+    'zh-CN': '蛙人',
+    'en-US': 'Bullywug',
+  },
+  centaur: {
+    'zh-CN': '半人马',
+    'en-US': 'Centaur',
+  },
   cleric: {
     'zh-CN': '牧师',
     'en-US': 'Cleric',
@@ -92,9 +104,29 @@ const TAG_LABELS: Record<string, { 'zh-CN': string; 'en-US': string }> = {
     'zh-CN': '混乱',
     'en-US': 'Chaotic',
   },
+  companion: {
+    'zh-CN': '伙伴',
+    'en-US': 'Companion',
+  },
+  control_fumble: {
+    'zh-CN': '失手控制',
+    'en-US': 'Fumble Control',
+  },
   control_knockback: {
     'zh-CN': '击退控制',
     'en-US': 'Knockback Control',
+  },
+  control_pacify: {
+    'zh-CN': '安抚控制',
+    'en-US': 'Pacify Control',
+  },
+  control_reposition: {
+    'zh-CN': '位移控制',
+    'en-US': 'Reposition Control',
+  },
+  control_root: {
+    'zh-CN': '定身控制',
+    'en-US': 'Root Control',
   },
   control_slow: {
     'zh-CN': '减速控制',
@@ -103,6 +135,10 @@ const TAG_LABELS: Record<string, { 'zh-CN': string; 'en-US': string }> = {
   control_stun: {
     'zh-CN': '眩晕控制',
     'en-US': 'Stun Control',
+  },
+  control_weaken: {
+    'zh-CN': '弱化控制',
+    'en-US': 'Weaken Control',
   },
   core: {
     'zh-CN': '核心英雄',
@@ -123,6 +159,10 @@ const TAG_LABELS: Record<string, { 'zh-CN': string; 'en-US': string }> = {
   dwarf: {
     'zh-CN': '矮人',
     'en-US': 'Dwarf',
+  },
+  eladrin: {
+    'zh-CN': '艾拉卓精灵',
+    'en-US': 'Eladrin',
   },
   elf: {
     'zh-CN': '精灵',
@@ -148,9 +188,17 @@ const TAG_LABELS: Record<string, { 'zh-CN': string; 'en-US': string }> = {
     'zh-CN': '战士',
     'en-US': 'Fighter',
   },
+  genasi: {
+    'zh-CN': '元素裔',
+    'en-US': 'Genasi',
+  },
   geneutral: {
     'zh-CN': '善恶中立',
     'en-US': 'Neutral (Good/Evil)',
+  },
+  gith: {
+    'zh-CN': '吉斯',
+    'en-US': 'Gith',
   },
   gnome: {
     'zh-CN': '侏儒',
@@ -204,6 +252,14 @@ const TAG_LABELS: Record<string, { 'zh-CN': string; 'en-US': string }> = {
     'zh-CN': '中立',
     'en-US': 'Neutral',
   },
+  lizardfolk: {
+    'zh-CN': '蜥蜴人',
+    'en-US': 'Lizardfolk',
+  },
+  lycanthrope: {
+    'zh-CN': '狼人',
+    'en-US': 'Lycanthrope',
+  },
   paladin: {
     'zh-CN': '圣武士',
     'en-US': 'Paladin',
@@ -228,13 +284,93 @@ const TAG_LABELS: Record<string, { 'zh-CN': string; 'en-US': string }> = {
     'zh-CN': '术士',
     'en-US': 'Sorcerer',
   },
+  spec_breaking: {
+    'zh-CN': '破坏专精',
+    'en-US': 'Breaking Spec',
+  },
   spec_control: {
     'zh-CN': '专精控制',
     'en-US': 'Spec Control',
   },
+  spec_control_fumble: {
+    'zh-CN': '失手控制专精',
+    'en-US': 'Fumble Control Spec',
+  },
+  spec_control_knockback: {
+    'zh-CN': '击退控制专精',
+    'en-US': 'Knockback Control Spec',
+  },
+  spec_control_slow: {
+    'zh-CN': '减速控制专精',
+    'en-US': 'Slow Control Spec',
+  },
+  spec_control_stun: {
+    'zh-CN': '眩晕控制专精',
+    'en-US': 'Stun Control Spec',
+  },
+  spec_debuff: {
+    'zh-CN': '减益专精',
+    'en-US': 'Debuff Spec',
+  },
+  spec_gold: {
+    'zh-CN': '金币专精',
+    'en-US': 'Gold Spec',
+  },
+  spec_hunter: {
+    'zh-CN': '猎杀专精',
+    'en-US': 'Hunter Spec',
+  },
+  spec_hunter_abberation: {
+    'zh-CN': '异怪猎杀',
+    'en-US': 'Aberration Hunter',
+  },
+  spec_hunter_beast: {
+    'zh-CN': '野兽猎杀',
+    'en-US': 'Beast Hunter',
+  },
+  spec_hunter_dragon: {
+    'zh-CN': '龙类猎杀',
+    'en-US': 'Dragon Hunter',
+  },
+  spec_hunter_fey: {
+    'zh-CN': '精类猎杀',
+    'en-US': 'Fey Hunter',
+  },
+  spec_hunter_fiend: {
+    'zh-CN': '邪魔猎杀',
+    'en-US': 'Fiend Hunter',
+  },
+  spec_hunter_fiends: {
+    'zh-CN': '邪魔猎杀',
+    'en-US': 'Fiends Hunter',
+  },
+  spec_hunter_giant: {
+    'zh-CN': '巨人猎杀',
+    'en-US': 'Giant Hunter',
+  },
+  spec_hunter_humanoid: {
+    'zh-CN': '类人生物猎杀',
+    'en-US': 'Humanoid Hunter',
+  },
+  spec_hunter_monstrosity: {
+    'zh-CN': '怪异猎杀',
+    'en-US': 'Monstrosity Hunter',
+  },
+  spec_hunter_undead: {
+    'zh-CN': '亡灵猎杀',
+    'en-US': 'Undead Hunter',
+  },
+  spec_speed: {
+    'zh-CN': '速度专精',
+    'en-US': 'Speed Spec',
+  },
   starter: {
     'zh-CN': '起始英雄',
     'en-US': 'Starter Champion',
+  },
+  tabaxi: {
+    'zh-CN': '塔巴西',
+    'en-US': 'Tabaxi',
   },
   tales: {
     'zh-CN': '传说堂',
@@ -252,6 +388,10 @@ const TAG_LABELS: Record<string, { 'zh-CN': string; 'en-US': string }> = {
     'zh-CN': '无联动队伍',
     'en-US': 'Unaffiliated',
   },
+  undead: {
+    'zh-CN': '亡灵',
+    'en-US': 'Undead',
+  },
   warforged: {
     'zh-CN': '战铸者',
     'en-US': 'Warforged',
@@ -263,6 +403,10 @@ const TAG_LABELS: Record<string, { 'zh-CN': string; 'en-US': string }> = {
   wizard: {
     'zh-CN': '法师',
     'en-US': 'Wizard',
+  },
+  'yuan-ti': {
+    'zh-CN': '蛇人',
+    'en-US': 'Yuan-Ti',
   },
 }
 
